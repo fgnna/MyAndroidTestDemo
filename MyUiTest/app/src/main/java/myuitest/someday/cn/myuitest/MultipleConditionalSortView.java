daa0cbf2691d90188e60059ee8446d8df694fffc
shaojunjie <fgnna@qq.com>
2017-05-15 16:16:20 +0800

    去除无用的类引用

--- MyUiTest/app/src/main/java/myuitest/someday/cn/myuitest/MultipleConditionalSortView.java
+++ MyUiTest/app/src/main/java/myuitest/someday/cn/myuitest/MultipleConditionalSortView.java
@@ -4,13 +4,11 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
